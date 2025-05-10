425e813d11eeb01e87b5b5093a1646d02e3e0012
dzyanino <dzyanino@gmail.com>
2025-05-10 23:15:55 +0300

    Feat : Server listening on `:43214`
    With :
    - App creation with `ID` for preference saving
    - Dynamic time on the top
    - System tray show up on app closing

--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	yt_go_server "yt-go/server"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 )
 
+/* It's just good to see a dynamic widget updating so fast each second :) */
+func updateTime(clock *widget.Label) {
+	var formatted = time.Now().Format("Current time is : 03:04:05")
+	clock.SetText(formatted)
+}
+
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
+	a := app.NewWithID("com.yt-go.dev")
+	w := a.NewWindow("Yt-Go")
 
-	fmt.Println("Here")
+	var clock = widget.NewLabel("clock_content")
 
-	w.SetContent(widget.NewLabel("Hello Fyne!"))
-	w.Resize(fyne.NewSize(300, 200))
+	/* Initializes a Vertival Layout Container */
+	var mainWindowContent *fyne.Container = container.NewVBox(
+		clock,
+	)
+
+	/*
+	* Updates the time displayed each second
+	* Background process using Goroutines `go func() {}()`
+	 */
+	go func() {
+		for range time.Tick(time.Second) {
+			fyne.Do(func() { updateTime(clock) })
+		}
+	}()
+
+	/* Applies content to the window and resizes it */
+	w.SetContent(mainWindowContent)
+	w.Resize(fyne.NewSize(800, 600))
+
+	/*
+	* Minimizes the app when closed
+	* System Tray depending on current OS
+	 */
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("Yt-go",
+			fyne.NewMenuItem("Show", func() {
+				w.Show()
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
+	w.SetCloseIntercept(func() {
+		w.Hide()
+	})
+
+	go func() { yt_go_server.StartServer() }()
 	w.ShowAndRun()
 }
